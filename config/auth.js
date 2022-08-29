404d5371064b6ffcd13508ec922c0e671cdb5cda
Bigfoot9999 <80417201+Bigfoot9999@users.noreply.github.com>
2022-08-29 02:28:48 +0000

    d

--- config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    ensureAuthenticated : function(req,res,next) {
-        if(req.isAuthenticated()) {
-            return next();
-        }
-        req.flash('error_msg' , 'please login to view this resource');
-        res.redirect('/users/login');
-    }
-}
